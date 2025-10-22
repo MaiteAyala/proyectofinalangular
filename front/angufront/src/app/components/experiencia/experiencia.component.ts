cf8f070faf6d72c1148cad2f4169f88c5368686a
MaiteAyala <maiteab67@gmail.com>
2025-10-22 10:08:32 -0500

    Delete front/angufront directory

--- front/angufront/src/app/components/experiencia/experiencia.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-experiencia',
-  standalone: true,
-  templateUrl: './experiencia.component.html',
-  styleUrls: ['./experiencia.component.css']
-})
-export class ExperienciaComponent {}
-
